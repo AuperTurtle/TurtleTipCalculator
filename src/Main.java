792320172c68ffaf89058400ef8ef6f1cd81b875
1E10 Laptop <sample@bths.edu>
2022-09-22 09:54:51 -0400

    .

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        DecimalFormat formatter = new DecimalFormat("#.##");
+
+
+
+    }
+}
