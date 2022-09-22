6fb0babd532e07084ece351a619afe39498f8c90
1E10 Laptop <sample@bths.edu>
2022-09-22 10:54:24 -0400

    .

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,34 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         DecimalFormat formatter = new DecimalFormat("#.##");
 
-        System.out.println("zzz");
+        System.out.println("Welcome to the tip calculator!");
+        System.out.println("How many people are in your group?");
+        int people = scan.nextInt();
+        scan.nextLine();
+        while (people < 1) {
+            System.out.println("Number of people has to be a positive and greater than zero, try again.");
+            people = scan.nextInt();
+        }
 
+        System.out.println("What's the tip percentage? (0-100):");
+        int tipPercent = scan.nextInt();
+        while ((tipPercent < -1) && (tipPercent > 101)) {
+            System.out.println("Tip percentage has to be between 0 and 100.");
+            tipPercent = scan.nextInt();
+
+        }
+
+        double checkEnd = 0;
+        double totalCost = 0;
+        while (checkEnd != -1) {
+            System.out.println("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end):");
+            checkEnd = scan.nextDouble();
+            String checkEndRounded = formatter.format(checkEnd);
+            if (checkEnd > 0) {
+                totalCost = (totalCost + checkEndRounded);
+                System.out.println(totalCost);
+            }
+        }
 
 
     }
